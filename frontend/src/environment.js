0b4e3da179300587c079b37942f4fcfa0fbf4e5a
Subhakar Maurya <subhakarmauryas@gmail.com>
2024-11-20 23:18:00 +0530

    second

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,11 +1,5 @@
-// let IS_PROD = true;
-const server = "http://localhost:3000"
-
-
-// IS_PROD ?
-//     "" :
-
-//     "http://localhost:8000"
-
+let IS_PROD = true;
+const server = IS_PROD ?
+    "https://liveconnectbackend.onrender.com" : "http://localhost:8000"
 
 export default server;
